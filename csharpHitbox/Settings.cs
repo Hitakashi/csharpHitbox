3780f1dd8bbae7a3a70eac92613c69aae0d733b5
Alexander DiMichele <whfalex@gmail.com>
2015-02-06 18:37:25 -0600

    Testing Git Ignore

--- csharpHitbox/Settings.cs
+++ csharpHitbox/Settings.cs
@@ -16,7 +16,7 @@ namespace csharpHitbox
         // Bot Username & Password
         public static String USERNAME = "";
         public static String PASSWORD = "";
-        public static String HEXCOLOR = "FF00FF";
+        public static String HEXCOLOR = "FF00FA";
 
         // Don't fill in
         public static String AUTH = "";
