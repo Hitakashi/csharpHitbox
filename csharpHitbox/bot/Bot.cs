275aea6e4ac527534343f7c171da7f8041ea197a
Alexander DiMichele <whfalex@gmail.com>
2015-02-12 01:11:06 -0600

    Improve Bot.RestartBot method.

--- csharpHitbox/bot/Bot.cs
+++ csharpHitbox/bot/Bot.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Linq;
 using csharpHitbox.api;
 using csharpHitbox.client;
 using csharpHitbox.utils;
@@ -15,8 +16,8 @@ namespace csharpHitbox.bot
     public class Bot
     {
         public static ConcurrentDictionary<String, Client> Clients = new ConcurrentDictionary<String, Client>();
-        public static Logger logger = new Logger();
         private static Bot instance;
+        private static Boolean canJoin = true;
 
         public Bot()
         {
@@ -38,8 +39,11 @@ namespace csharpHitbox.bot
             AddClient(channel);
         }
 
-        public static void AddClient(String channel)
+        // This should ONLY be used on startup or anything else that doesn't run within a channel.
+        private static void AddClient(String channel)
         {
+            if (!canJoin)
+                return;
             try
             {
                 Logger.Log("Creating Client@" + channel);
@@ -69,6 +73,48 @@ namespace csharpHitbox.bot
                 Console.Error.WriteLine(e);
             }
         }
+
+
+        // This should be used for !join commands or anything similar so we can tell users that they cannot join.
+        public static void AddClient(Client client2, String channel)
+        {
+            if (!canJoin)
+            {
+                MessageHandler.SendMessage(client2,
+                    "I cannot join channels at this time. Please try again in a few minutes.");
+                return;
+            }
+
+            try
+                {
+                    Logger.Log("Creating Client@" + channel);
+                    Client client = new Client(channel.ToLower());
+                    Clients.TryAdd(channel.ToLower(), client);
+                    try
+                    {
+                        client.Connect();
+                        //if (httpServer != null) return;
+                        //httpServer = new MyHttpServer(80);
+
+                        //Thread thread = new Thread((httpServer.listen));
+                        //thread.Start();
+
+                        //httpServer = new Listener();
+                        if (Settings.APISERVER)
+                            Program.Main();
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.Error.WriteLine(e);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine(e);
+                }
+        }
+
         public static void RemoveClient(String channel)
         {
             if (Clients.ContainsKey(channel))
@@ -90,28 +136,51 @@ namespace csharpHitbox.bot
                     MessageHandler.SendQuitMessage(client);
                     client.Close();
                 }
-                catch (Exception ignored)
+                catch (Exception)
                 {
 
                 }
             }
+            Environment.Exit(0);
         }
 
         public static void RestartBot()
         {
+            // We need to disallow joining to channels until the restart is done.
+            canJoin = false;
+            // We need to copy the original clients before removing all of them.
+            ConcurrentDictionary<String, Client> a = Clients;
+
             foreach (Client client in Clients.Values)
             {
                 try
                 {
+                    // Let default channels stay in the bot.  Usually the default channel is used for the bots home for join commands.
+                    if (Settings.CHANNEL.Contains(client.GetChannel()))
+                        continue;
                     MessageHandler.SendQuitMessage(client);
                     RemoveClient(client.GetChannel());
-                    AddClient(client.GetChannel());
                 }
-                catch (Exception ignoredException)
+                catch (Exception)
                 {
                     
                 }
             }
+
+            // Add
+
+            // Now we need to loop the copy of Clients and add them back.
+
+            foreach (String clients in a.Keys)
+            {
+                if (Settings.CHANNEL.Contains(clients))
+                    continue;
+                AddClient(clients);
+            }
+
+            // Just in case. Want GC to take care of this ASAP
+            a.Clear();
+            canJoin = true;
         }
     }
 }
